48ae65d30eab0cbbcc69582756b97b230ec481ae
Kabbs19 <77408435+Kabbs19@users.noreply.github.com>
2026-05-11 22:16:56 +0100

    Add first batch of js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-js
+
+const numberInput = document.getElementById("number-input")
+const inputValues = document.querySelectorAll(".input-value")
+const convertBtn = document.getElementById("convert-btn") 
+const meters = document.getElementById("meters-el")
+const feet = document.getElementById("feet-el")
+const liters = document.getElementById("liters-el")
+const gallons = document.getElementById("gallons-el")
+const kilograms = document.getElementById("kilograms-el")
+const pounds = document.getElementById("pounds-el")
+
+numberInput.addEventListener("input", convertNumber)
+
+function convertNumber() {
+    const number = Number(numberInput.value)
+
+    inputValues.forEach(el => {
+        el.innerHTML = `${number}`
+    })
+
+    meters.innerHTML = `${(number / 3.281).toFixed(3)}`
+    feet.innerHTML = `${(number * 3.281).toFixed(3)}`
+    liters.innerHTML = `${(number * 4.546).toFixed(3)}`
+    gallons.innerHTML = `${(number / 4.546).toFixed(3)}`
+}
